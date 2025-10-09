ad1ac50c87cb920919c90f765caa67a9d51b0cb8
Nico Jansen <jansennico@gmail.com>
2025-10-09 16:06:48 +0200

    Make mutant survive in the numers.js code base

--- uva-2025/labs/assignment/numbers.js/test/basic.test.js
+++ uva-2025/labs/assignment/numbers.js/test/basic.test.js
@@ -36,7 +36,7 @@ suite("numbers", () =>  {
 
   // basic.substraction
   test("subtraction should return the difference of items in an array", () => {
-    assert.equal(basic.subtraction([5, 3, 1, -9]), 10);
+    assert.equal(basic.subtraction([0, 5, 3, 1, -9]), 0);
   });
 
   test("subtraction should throw an exception when given anything but an array", () => {
